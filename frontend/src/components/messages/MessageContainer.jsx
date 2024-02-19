b4000a75eaf89f67fcac61d65f39cad34a773291
riaz101 <mriaz.sattar@gmail.com>
2024-02-19 18:20:19 +0500

    aaa

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -36,7 +36,7 @@ export default MessageContainer;
 const NoChatSelected = () => {
 	const { authUser } = useAuthContext();
 	return (
-		<div className='flex items-center justify-center w-full h-full'>
+		<div className='flex items-center justify-center min-w-36 mx-auto h-full'>
 			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
 				<p>Welcome 👋 {authUser.fullName} ❄</p>
 				<p>Select a chat to start messaging</p>
